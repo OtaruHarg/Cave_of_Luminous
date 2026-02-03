53442fb384c6223e62c1ec2e59fc4aaff13a7732
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-02-03 09:54:36 +0900

    Update script_passset.js

--- script_passset.js
+++ script_passset.js
@@ -5,8 +5,8 @@ window.onload = load();
 function load(){
   x = 1;
   while(x == 1){
-    test = Number(window.prompt("6桁のパスワードを入力してください。"));
-    if(test < 1000000 & test >= 100000){
+    test = Number(window.prompt("5桁のパスワードを入力してください。"));
+    if(test < 100000 & test >= 10000){
       x = 2;
       key = "pl_pass";
       val = test;
